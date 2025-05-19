0b759c0988db618a9c2ab2fc07c51dfb477d6287
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-05-19 11:09:46 +0200

    Recurrence enum

--- app/src/main/java/com/example/perpetualcalendar/Event.kt
+++ app/src/main/java/com/example/perpetualcalendar/Event.kt
@@ -3,10 +3,18 @@ package com.example.perpetualcalendar
 import java.util.UUID
 import java.time.LocalDateTime
 
+enum class Recurrence {
+    NONE,
+    DAILY,
+    WEEKLY,
+    MONTHLY
+}
+
 data class Event(
     val id: String = UUID.randomUUID().toString(),
     val title: String,
     val startDateTime: LocalDateTime,
     val endDateTime: LocalDateTime,
-    val description: String = ""
+    val description: String = "",
+    val recurrence :Recurrence = Recurrence.NONE
 )
